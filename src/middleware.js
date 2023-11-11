efe7764f13d8c6f12ea0a1f617a7f3b6b9a7fbe8
malizkl <muhammedaliozkul55@gmail.com>
2023-11-11 23:34:46 +0300

    refactor

---
+++ src/middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(request) {
+  const device = userAgent(request);
+  const viewport = device.type === 'mobile' ? 'mobile' : 'desktop';
+
+  const url = request.nextUrl.clone(); // Clone the URL so we can modify it
+  url.searchParams.set('viewport', viewport);
+
+  return NextResponse.rewrite(url);
+}
