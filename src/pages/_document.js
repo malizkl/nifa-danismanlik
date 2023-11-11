ab9970b3dfdc30ec6ab944604ec085366492316d
malizkl <muhammedaliozkul55@gmail.com>
2023-11-11 23:27:16 +0300

    refactor

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,15 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta
-         name="viewport"
-         content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        ></meta>
-      </Head>
-      
-      
-            <body>
+      <Head/>
+      <body>
         <Main />
         <NextScript />
       </body>
